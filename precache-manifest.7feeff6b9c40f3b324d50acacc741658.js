aed94e588c1a0a2840c2cf9d6756314876415e2d
Emanuelpna <emanuelpna@yahoo.com.br>
2020-03-14 18:57:59 -0300

    initial commit

---
+++ precache-manifest.7feeff6b9c40f3b324d50acacc741658.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34b0d0ee6df97fc8bd710ca12e4bf276",
+    "url": "/css-utilities/index.html"
+  },
+  {
+    "revision": "c17f16ace9a31f2eb12e",
+    "url": "/css-utilities/static/css/main.2daeb4fa.chunk.css"
+  },
+  {
+    "revision": "bceae5b8b54ca9e35962",
+    "url": "/css-utilities/static/js/2.9496f881.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/css-utilities/static/js/2.9496f881.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c17f16ace9a31f2eb12e",
+    "url": "/css-utilities/static/js/main.867bce31.chunk.js"
+  },
+  {
+    "revision": "71eb5687ae37d916365d",
+    "url": "/css-utilities/static/js/runtime-main.b5170f6b.js"
+  },
+  {
+    "revision": "fecc594e4254f024721f18b876d1cc95",
+    "url": "/css-utilities/static/media/droplet.fecc594e.svg"
+  },
+  {
+    "revision": "04be56217db0721f1952571425be3b6f",
+    "url": "/css-utilities/static/media/square.04be5621.svg"
+  },
+  {
+    "revision": "dfbba03b0bcc8fd421f289cd27644405",
+    "url": "/css-utilities/static/media/triangle.dfbba03b.svg"
+  },
+  {
+    "revision": "a8947bb52bcf38bd5b3a72e224da9736",
+    "url": "/css-utilities/static/media/zap-off.a8947bb5.svg"
+  },
+  {
+    "revision": "610c57ea099bb281d3bbb3e11ad011b4",
+    "url": "/css-utilities/static/media/zap.610c57ea.svg"
+  }
+]);
